10c39fca74be6922d1f65514aaba5d147a4f3cbb
quintonwall <quinton_wall@hotmail.com>
2021-11-15 09:33:37 -0800

    pointed to trial account

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css';
+import { Route, NavLink, Routes} from 'react-router-dom';
+import StoreSales from './Components/storesales';
+import About from './Components/about';
+import { Liveboard } from './Components/storeembed';
+
+// ThoughtSpot
+import {
+  AuthType,
+  init,
+  Page,
+} from "@thoughtspot/visual-embed-sdk";
+
+const TSURL = "https://try.thoughtspot.cloud";
+
+function App() {
+
+  init({
+    thoughtSpotHost: TSURL,
+    authType: AuthType.None
+  });
+
+  return (
+    /*
+    <div>
+      hello liveboard
+      <Liveboard />
+    </div>
+    */
+    <div className="App">
+       <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/storesales" element={<StoreSales />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <div className="list">
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/storesales">Store Sales</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+        </ul>
+      </div>
+  </div> 
+  
+  );
+}
+
+export default App;
---
+++ src/Components/storesales.js
@@ -0,0 +1,20 @@
+import { PinboardEmbed } from "@thoughtspot/visual-embed-sdk";
+
+
+
+function RenderLiveboard(props) {
+    return <PinboardEmbed frameParams={{height: "80vw"}} pinboardId={"e4968d70-1c03-4ca4-8081-894a3a0301c1"}/>
+}
+/* try.thoughtspot pin: 41dd8d51-83ad-47ae-bbd1-801439319b0e */
+/* internal cluster pin: e4968d70-1c03-4ca4-8081-894a3a0301c1 */
+
+export default function StoreSales() {
+    return (
+        <div>
+            <h1>Store Sales</h1>
+            <PinboardEmbed frameParams={{height: "80vw"}} 
+                        pinboardId={"41dd8d51-83ad-47ae-bbd1-801439319b0e"}/>
+        </div>
+            
+    );
+}
